58bd9d7acfe8a842a2a0a0536e6634cd32677ec6
Keertiraj Patil <keerti4p@gmail.com>
2016-03-30 16:33:30 +0200

    Initial Commit

---
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+	<h1>Hello Spring MVC</h1>
+</body>
+</html>
---
+++ src/test/java/com/example/DemoSpringWebApplicationTests.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.demo.DemoSpringWebApplication;
+
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DemoSpringWebApplication.class)
+@WebAppConfiguration
+public class DemoSpringWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+	
+	@Test
+	public void testApp()
+	{
+		 assertTrue (21 < 3);
+	}
+
+}
