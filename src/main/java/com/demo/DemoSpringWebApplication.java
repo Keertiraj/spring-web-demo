aa6c63effc039b3053b1951a10d406d59ef58955
Keertiraj Patil <keerti4p@gmail.com>
2016-04-06 14:19:42 +0200

    new changes

--- src/main/java/com/demo/DemoSpringWebApplication.java
+++ src/main/java/com/demo/DemoSpringWebApplication.java
@@ -11,7 +11,7 @@ public class DemoSpringWebApplication {
 	
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "Heeeeeeeeeeello Docker World";
 	}
 
 	public static void main(String[] args) {
