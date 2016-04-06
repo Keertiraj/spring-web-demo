d44643a3b2ea779469703ac3b206136d8c87aa72
Keertiraj Patil <keerti4p@gmail.com>
2016-04-06 11:06:39 +0200

    changes to project

--- src/main/java/com/demo/controller/HomeController.java
+++ src/main/java/com/demo/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 	
 	@RequestMapping("/test")
 	public String home() {
-		return "Hello Docker World again";
+		return "home";
 	}
 }
